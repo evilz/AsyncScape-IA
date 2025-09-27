c57e48b779b336442bb17082503354fdaee27487
Vincent B. <evilznet@gmail.com>
2025-09-27 17:13:47 +0200

    chore: clean build artifacts

--- Aspire.AppHost/AppHost.cs
+++ Aspire.AppHost/AppHost.cs
@@ -1,3 +1,16 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("catalog-db")
+                      .WithDataVolume();
+
+var catalogDatabase = postgres.AddDatabase("catalog");
+
+builder.AddProject<Projects.AsyncScapeIA_WebApp>("webapp")
+       .WithReference(catalogDatabase);
+
+builder.AddProject<Projects.AsyncScapeIA_Workers>("worker")
+       .WithReference(catalogDatabase);
 
 builder.Build().Run();
